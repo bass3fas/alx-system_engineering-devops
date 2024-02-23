90bf2f64bfea1ddf61745c9e5a068bb3d0d439a4
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-02-23 02:22:27 -0800

    br7tak

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 #!/usr/bin/pup
-# Install a specific version of Flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  require => Package['python3-pip'],
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
